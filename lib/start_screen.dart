d3ee66414e40f1429dd5ce61e5a11dd05e271b9a
Alex Kumancev <kumancev1@gmail.com>
2023-04-13 16:07:47 +0300

    add input addresses container

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -2,15 +2,10 @@ import 'package:battle_men_and_women/fortune_wheel_screen.dart';
 import 'package:battle_men_and_women/services/screen_navigation_const.dart';
 import 'package:flutter/material.dart';
 
-List<Color> colors = [
-  Colors.redAccent,
-  Colors.greenAccent,
-  Colors.blueAccent,
-  Colors.amberAccent,
-];
+List<Color> colors = [  Colors.redAccent,  Colors.greenAccent,  Colors.blueAccent,  Colors.amberAccent,];
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -53,13 +48,48 @@ class StartScreen extends StatelessWidget {
                         color: Colors.black),
                   ])),
               const SizedBox(height: 50),
+              // Container for BSC Address Inputs
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Enter Address 1',
+                        ),
+                        keyboardType: TextInputType.text,
+                        onChanged: (value) {
+                          // TODO: Store value somewhere
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Enter Address 2',
+                        ),
+                        keyboardType: TextInputType.text,
+                        onChanged: (value) {
+                          // TODO: Store value somewhere
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
               ElevatedButton(
                 onPressed: () {
                   changeScreen(context, const FortuneWheelScreen());
                 },
                 style: buttonStyle,
                 child: const Text('Play', style: TextStyle(fontSize: 25)),
-              )
+              ),
+              const SizedBox(height: 32),
             ],
           ),
         ),
